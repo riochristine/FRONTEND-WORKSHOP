801552d830e0b335d5e5db642577e8253d3ac389
riochristine <riochristineb@gmail.com>
2025-02-17 15:15:39 +0800

    Create temp-converter

---
+++ Exercises/JavaScript-Exercises/Basic/Basic-JS-Exercise-3/temp-converter.js
@@ -0,0 +1,29 @@
+const convertButton = document.querySelector("button");
+
+convertButton.addEventListener("click", function callback(){
+    const temp = parseFloat(document.getElementById("temp").value);
+
+    if (isNaN(temp)) {
+        alert("Invalid input. Please enter a valid number for temperature.");
+        return; // Exit function early if input is invalid
+    }
+
+    const unit = document.getElementById("unit").value;
+    
+    let convertedTemp;
+    let unitSymbol;
+    if (unit === 'celsius') {
+        convertedTemp = (temp - 32) * 5/9; // f to c
+        unitSymbol = 'C'
+    } else if (unit === 'fahrenheit') {
+        convertedTemp = (temp * 9/5) + 32; // c to f
+        unitSymbol = 'F'
+    } else {
+        alert("Please select a unit to convert into.");
+        return; // Exit early if no unit is selected
+    }
+
+    document.getElementById("conversion-text").textContent = `Converted Temperature: ${convertedTemp.toFixed(2)} °${unitSymbol}`;
+
+    console.log(`Converted Temperature: ${convertedTemp.toFixed(2)} °${unitSymbol}`);
+});
